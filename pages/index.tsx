4840b5e98f941366afaed5c68194f34e70fe644a
Jerain Choi <choi.jerasin@gmail.com>
2025-02-05 14:04:18 +0700

    refactor: fix root page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
+'use client'
+
 import Link from "next/link";
+import { redirect, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  
+  useEffect(() => {
+    // ทำการ redirect ไปยังหน้าใหม่หลังจากที่หน้าโหลดเสร็จแล้ว
+    router.push('/pokemon');
+  }, [router]);
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-5 lg:p-24">
-      <h1
+      {/* <h1
         className="my-14 text-center text-3xl font-bold uppercase 
     tracking-widest"
       >
@@ -18,7 +30,9 @@ export default function Home() {
         <li>
           <Link href="/login">Login Page</Link>
         </li>
-      </ul>
+      </ul> */}
+
+
     </main>
   );
 }
