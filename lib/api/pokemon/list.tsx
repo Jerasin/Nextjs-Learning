dd663cff317a6f1f8ee055e375bb3536a00a558d
Jerain Choi <choi.jerasin@gmail.com>
2025-02-07 16:08:05 +0700

    test: add env

--- lib/api/pokemon/list.tsx
+++ lib/api/pokemon/list.tsx
@@ -13,7 +13,7 @@ export const GetPokemonList = (
 
   console.log("process.env.NEXT_PUBLIC_API_URL",process.env.NEXT_PUBLIC_API_URL);
 
-  const url = `${process.env.NEXT_PUBLIC_API_URL}?limit=${limit}&offset=${offset}`;
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/pokemon?limit=${limit}&offset=${offset}`;
   const { data, error, isLoading } = useSWR(url, fetcher);
 
   return {
